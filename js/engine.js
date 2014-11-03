0fc07ddda78b7dfc9413cdf98e2e448a4f719da1
intheon <allobon@gmail.com>
2014-11-03 14:02:25 +0000

    its very dynamic
    add rss reader

--- js/engine.js
+++ js/engine.js
@@ -1,12 +1,59 @@
-var links = [
-	"<a href='index.html'>home</a>",
-	"<a href='mail.html'>mail</a>",
-	"<a href='entertainment.html'>entertainment</a>",
-	"<a href='workout.html'>workout</a>",
-];
+// ****************
+// VIEW CONTROLLERS
+// ****************
+	var links = [
+		"<a href='index.html'>home</a>",
+		"<a href='mail.html'>mail</a>",
+		"<a href='entertainment.html'>entertainment</a>",
+		"<a href='workout.html'>workout</a>",
+	];
+
+	for (i = 0; i <= links.length - 1; i++)
+	{
+		$(".menu_panel").append("<div class='menu_item'>" + links[i] + "</div>");
+	}
+// the above just dynamically adds in links 
+// because i cant be bothered to hard code them
+
+	function detectChanges(changes)
+	{
+		$("#"+changes[0].name).append(xmlResponses[changes[0].name]);
+	}
 
 
-for (i = 0; i <= links.length - 1; i++)
-{
-	$(".menu_panel").append("<div class='menu_item'>" + links[i] + "</div>");
-}
+// *********
+// RSS MODEL
+// *********
+	function getFeed(websiteUrl,tag)
+	{
+		$.ajax({
+			url: "http://localhost/webshirt/php/module_get_feed.php",
+			type: "POST",
+			data: {
+				website: websiteUrl
+			},
+
+			success: function(response)
+			{
+				xmlResponses[tag] = response
+			}
+		});
+	}
+// this accepts urls of valid feeds and sends 
+// them to a php module_get_feed
+	var xmlResponses = {};
+	Object.observe(xmlResponses,function(changes){
+		detectChanges(changes);
+	});
+// the response does get put in it's own object
+// it even has a listener to perform a callback (function) when it changes!
+
+// **************
+// INITIALISATION
+// **************
+$( document ).ready(function(){
+	getFeed("http://www.gamespot.com/feeds/reviews/","gamespot");
+	getFeed("http://feeds.bbci.co.uk/news/technology/rss.xml","bbc");
+});
+
+
---
+++ php/module_get_feed.php
@@ -0,0 +1,26 @@
+<?php
+// this will read xml and send it back to javascript
+// it takes an argument of a url of a valid site that serves xml
+// copyright 2014 intheon
+$entries = array();
+
+if (isset($_POST['website']))
+{
+	$url_param = $_POST['website'];
+	// once we have the url parameter, process to parse the biatch.
+
+	$xml = simplexml_load_file($url_param);
+	// simplexml_load_file is useful as it will just get the thing we want
+
+	foreach($xml as $key)
+	{
+		$entries = array_merge($entries, $xml->xpath("//item"));
+	}
+	// i get back an object
+	// the above loops through, and merges each entry onto an empty array
+	// the magic is in the xpath, it digs into the xml and grabs only those elements and children
+}
+
+echo json_encode($entries);
+// give it back to js
+?>
